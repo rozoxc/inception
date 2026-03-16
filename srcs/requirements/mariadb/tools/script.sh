6755112603c4cf3c28d29a32b12599808a08a966
r0z0 <abdel.brave@gmail.com>
2026-03-16 03:14:50 -0400

    ld

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,12 @@
+
+#db_name = Database Name
+#db_user = User
+#db_pwd = User Password
+
+echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+
+mysql < db1.sql
